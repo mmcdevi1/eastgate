98f1e911e5ce1e03b15ff99ab79fad1beddb991a
Michael McDevitt <mmcdevi1@gmail.com>
2015-04-20 12:01:29 -0400

    devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  devise_scope :user do
+    get  '/register', to: 'devise/registrations#new', as: :register
+    get  '/profile/edit', to: 'devise/registrations#edit', as: :edit
+
+    get  '/login', to: 'devise/sessions#new', as: :login   
+    get  '/logout', to: 'devise/sessions#destroy', as: :logout
+  end  
+
   resources 'static_pages', only: [:index]
   root to: 'static_pages#index'
+
 end
