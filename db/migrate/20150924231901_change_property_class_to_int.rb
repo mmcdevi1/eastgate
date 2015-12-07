6edd6d1aee0f969453fdf7e8ce0412dae9fd85c8
Michael McDevitt <mmcdevi1@gmail.com>
2015-12-07 16:30:10 -0500

    more migration

--- db/migrate/20150924231901_change_property_class_to_int.rb
+++ db/migrate/20150924231901_change_property_class_to_int.rb
@@ -1,5 +1,5 @@
 class ChangePropertyClassToInt < ActiveRecord::Migration
   def change
-    change_column :assets, :property_class, :integer
+    change_column :assets, :property_class, 'integer USING CAST(property_class AS integer)'
   end
 end
