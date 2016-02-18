3e26ba98a7991327149102cc3a3e659deeb67647
Michael McDevitt <mmcdevi1@gmail.com>
2016-02-18 10:05:05 -0500

    fix hatch index route

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -49,7 +49,7 @@ module Admin
 
     def user_params
       params.require(:user).permit(
-          :email, :password, :password_confirmation, :username, :first_name, :last_name, :client_id, :admin
+          :email, :password, :password_confirmation, :username, :first_name, :last_name, :client_id, :admin, :broker
         )
     end
 
