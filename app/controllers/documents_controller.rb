7b98f44259e3f852778d618752f784737a010d9c
Michael McDevitt <mmcdevi1@gmail.com>
2016-02-19 09:36:35 -0500

    fix

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,4 +1,5 @@
 require 'zipruby'
+require 'uri'
 
 class DocumentsController < HatchesController
   before_action :correct_client
@@ -28,7 +29,7 @@ class DocumentsController < HatchesController
         if folder.is_root?
           ar.add_dir( "#{folder.name}" )
           folder.documents.each do |document|
-            data = Rails.env.development? ? document.uploaded_file.path : document.uploaded_file.url
+            data = Rails.env.development? ? document.uploaded_file.path : URI.encode(document.uploaded_file.url)
             ar.add_file("#{folder.name}/#{document.file_name}", data)
             # ar.add_file("#{folder.name}/#{document.uploaded_file.path}", data)
           end
