0d1abbb7bf6de38e74df87fe81a1ab85cf061601
Michael McDevitt <mmcdevi1@gmail.com>
2016-02-18 19:43:56 -0500

    fix

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -28,7 +28,7 @@ class DocumentsController < HatchesController
         if folder.is_root?
           ar.add_dir( "#{folder.name}" )
           folder.documents.each do |document|
-            data = Rails.env.development? ? document.uploaded_file.path : document.uploaded_file.path
+            data = Rails.env.development? ? document.uploaded_file.path : open( URI.parse( URI.encode(document.uploaded_file.url)))
             ar.add_file("#{folder.name}/#{document.file_name}", data)
           end
         else
@@ -39,7 +39,7 @@ class DocumentsController < HatchesController
           ar.add_dir( "#{result}#{folder.name}" )
 
           folder.documents.each do |document|
-            data = Rails.env.development? ? document.uploaded_file.path : document.uploaded_file.path
+            data = Rails.env.development? ? document.uploaded_file.path : open( URI.parse( URI.encode(document.uploaded_file.url)))
             ar.add_file("#{result}#{folder.name}/#{document.file_name}", data)
           end
         end
