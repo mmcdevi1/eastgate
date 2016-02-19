cd2500f24059bdce7d5059360e2bcb82acb7bdc0
Michael McDevitt <mmcdevi1@gmail.com>
2016-02-18 19:04:01 -0500

    test

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -49,7 +49,8 @@ class DocumentsController < HatchesController
                                 :disposition => 'attachment',
                                 :filename => file_name
     else
-      send_data temp_file.path, :filename => document.uploaded_file_file_name
+      data = open( URI.parse( URI.encode( document.uploaded_file.url ) ) )
+      send_data data, :filename => document.uploaded_file_file_name
     end
 
     temp_file.close
