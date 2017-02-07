0fbc1c323ca92f2950d50aa5bb65d11843920781
Michael McDevitt <mmcdevi1@gmail.com>
2017-02-07 13:50:41 -0500

    trying to fix video bg

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -44,8 +44,74 @@ $(function() {
   });
 });
 
+// function initVideos(){
+
+//     if ($('.player').length) {
+//       $('.player').each(function(index, el) {
+//         $(el).mb_YTPlayer({
+//           autoPlay: true,
+//           mute: true,
+//         });
+//       });
+
+//       if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+//         $('.video-container').append('<div class="fallback-bg"></div>');
+//         $('#fallback-bg').css('background-image', 'url('+$('.video-container').data('fallback-bg')+')');
+//       }
+
+//       if ($('.video-container').length > 1) {
+//         $('.video-container').css('position', 'absolute');
+//       }
+
+//       $('.video-container').each(function(index, el) {
+//         if ($(el).closest('section').find('#home-slider').length) {
+//           $(el).closest('section').find('#home-slider').css('background-color', 'transparent');
+//         }
+//       });
 
 
+//     }
+
+//     var videoEl = $('.video-container>video');
+
+//     var setProportion = function () {
+//       var proportion = getProportion();
+//       videoEl.width(proportion*1280);
+//       videoEl.height(proportion*780);
+
+//       centerVideo();
+//     }
+
+//     var getProportion = function () {
+//       var windowWidth = $(window).width();
+//       var windowHeight = $(window).height();
+//       var windowProportion = windowWidth / windowHeight;
+//       var origProportion = 1280 / 720;
+//       var proportion = windowHeight / 720;
+
+//       if (windowProportion >= origProportion) {
+//         proportion = windowWidth / 1280;
+//       }
+
+//       return proportion;
+//     }
+
+//     var centerVideo = function() {
+//       var centerX = (($(window).width() >> 1) - (videoEl.width() >> 1)) | 0;
+//       var centerY = (($(window).height() >> 1) - (videoEl.height() >> 1)) | 0;
+
+//       videoEl.css({ 'left': centerX, 'top': centerY });
+
+//     }
+
+//     if (videoEl.length) {
+//       $(window).resize(function() {
+//         setProportion();
+//       }).trigger('resize');
+//     }
+//   }
+
+// initVideos();
 
 
 
