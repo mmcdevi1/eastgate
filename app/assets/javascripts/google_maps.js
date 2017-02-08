7662b779035ab5931147e65778bdb8b3641fdcb7
Michael McDevitt <mmcdevi1@gmail.com>
2017-02-08 16:35:16 -0500

    trying to fix video bg

--- app/assets/javascripts/google_maps.js
+++ app/assets/javascripts/google_maps.js
@@ -1,7 +1,7 @@
 function initMap() {
   var uluru = {lat: 40.7622500, lng: -73.9723700};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 14,
+    zoom: 16,
     center: uluru
   });
   var marker = new google.maps.Marker({
