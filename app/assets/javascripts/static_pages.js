e6d696e2f6b30c7a5f3bdf487bdc7b0ff6b4a77d
Michael McDevitt <mmcdevi1@gmail.com>
2017-02-07 20:00:07 -0500

    trying to fix video bg

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -42,8 +42,7 @@ $(document).ready(function() {
   var bg = $(".video_poster");
 
 
-      bg.height($(window).height()*1.2);
-      bg.width($(window).width()*1.2);
+
 
 
   // $(window).resize(resizeBackground);
