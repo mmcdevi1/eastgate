558a717acf7f6e34b16ce2f128e343588ef6d929
Michael McDevitt <mmcdevi1@gmail.com>
2017-02-25 23:12:58 -0500

    fix some css

--- app/controllers/job_postings_controller.rb
+++ app/controllers/job_postings_controller.rb
@@ -2,11 +2,15 @@ class JobPostingsController < ApplicationController
   before_action :set_job_posting, only: [:show]
 
   def show
+    unless @job_posting
+      redirect_to career_applications_path
+      flash[:danger] = 'Record does not exist.'
+    end
   end
 
   private
   def set_job_posting
-    @job_posting = JobPosting.find(params[:id])
+    @job_posting = JobPosting.find_by_id(params[:id])
   end
 
 end
