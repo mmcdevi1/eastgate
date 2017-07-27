7810ae4d72b16a1872ab016e6109398c4b0a7402
Michael McDevitt <mmcdevi1@gmail.com>
2017-07-27 00:02:20 -0400

    redirect

--- app/controllers/job_postings_controller.rb
+++ app/controllers/job_postings_controller.rb
@@ -2,6 +2,13 @@ class JobPostingsController < ApplicationController
   before_action :set_job_posting, only: [:show]
 
   def show
+    if @job_posting
+      if @job_posting.published == false
+        redirect_to career_applications_path
+        flash[:danger] = 'Record does not exist.'
+      end
+    end
+
     unless @job_posting
       redirect_to career_applications_path
       flash[:danger] = 'Record does not exist.'
